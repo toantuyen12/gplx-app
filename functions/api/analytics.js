91852fe9b00ea4ded8438dd0406da6d81f67d36c
toantuyen <minhtoan2141@gmail.com>
2026-03-08 09:53:43 +0700

    Update analytics.js

--- functions/api/analytics.js
+++ functions/api/analytics.js
@@ -1,28 +1,12 @@
-export async function onRequest() {
+export async function onRequestGet() {
 
-const propertyId = "386855850";   // property ID của bạn
+const propertyId = "386855850"
 
-const url =
-`https://analyticsdata.googleapis.com/v1beta/properties/${propertyId}:runReport`;
-
-const body = {
-dateRanges: [{ startDate: "7daysAgo", endDate: "today" }],
-metrics: [{ name: "activeUsers" }]
-};
-
-const res = await fetch(url,{
-method:"POST",
-headers:{
-"Authorization": "Bearer YOUR_ACCESS_TOKEN",
-"Content-Type":"application/json"
-},
-body: JSON.stringify(body)
-});
-
-const data = await res.json();
-
-return new Response(JSON.stringify(data),{
-headers:{ "Content-Type":"application/json" }
-});
+return new Response(JSON.stringify({
+property: propertyId,
+status:"API running"
+}),{
+headers:{'content-type':'application/json'}
+})
 
 }
